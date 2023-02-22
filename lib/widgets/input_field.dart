12d2c7471050f97a855b842347ee7a8e7d56f859
Syed Raza Haider <97038058+syed-raza-haider@users.noreply.github.com>
2023-02-22 14:07:38 +0500

    Add input text field widget

---
+++ lib/widgets/input_field.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class InputField extends StatelessWidget {
+  const InputField(
+      {Key? key,
+      required this.textEditingController,
+      required this.title,
+      required this.hintText,
+      this.validator,
+      this.obsecure = false,
+      this.autovalidateMode,
+      this.keyboardType = null,
+      this.maxLines = 1,
+      this.minLines = null,
+      this.enabled = true,
+      this.textInputAction,
+      this.onSubmit})
+      : super(key: key);
+
+  final TextEditingController textEditingController;
+  final String title;
+  final String hintText;
+  final String? Function(String?)? validator;
+  final bool obsecure;
+  final AutovalidateMode? autovalidateMode;
+  final TextInputType? keyboardType;
+  final int? maxLines;
+  final int? minLines;
+  final bool enabled;
+  final TextInputAction? textInputAction;
+  final Function()? onSubmit;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Container(
+          color: Colors.white,
+          child: TextFormField(
+            // onEditingComplete: () => onSubmit != null ? onSubmit!() : null,
+            textInputAction: textInputAction,
+            enabled: enabled,
+            obscureText: obsecure ? true : false,
+            controller: textEditingController,
+            validator: validator,
+            autovalidateMode: autovalidateMode,
+            minLines: minLines,
+            maxLines: maxLines,
+            keyboardType: keyboardType,
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              hintText: hintText,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
