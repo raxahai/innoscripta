ce339849361fd8aec45938852334ae0b80b68033
Syed Raza Haider <97038058+syed-raza-haider@users.noreply.github.com>
2023-02-24 21:59:20 +0500

    Add message property to our Failure class

--- lib/core/failure/failure.dart
+++ lib/core/failure/failure.dart
@@ -2,4 +2,10 @@ import 'package:equatable/equatable.dart';
 
 /// Super class for all Failure types,
 /// e.g: [FirebaseAuthFailure] for all Firebase auth related exceptions
-abstract class Failure extends Equatable {}
+abstract class Failure extends Equatable {
+  final String? message;
+
+  const Failure(this.message);
+
+  String? get getMessage => message;
+}
--- lib/core/failure/firebase_auth_failure.dart
+++ lib/core/failure/firebase_auth_failure.dart
@@ -1,6 +1,8 @@
 import 'package:innoscripta/core/failure/failure.dart';
 
 class FirebaseAuthFailure extends Failure {
+  const FirebaseAuthFailure(String? message) : super(message);
+
   @override
   List<Object?> get props => [];
 }
